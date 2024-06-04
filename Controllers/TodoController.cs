af79ac13929974fff6f3e729bd07d237e1bea7f4
DANSKI423319 <dskillman@coreblue.com>
2024-06-04 21:24:22 +0100

    Add memory endpoints for todos

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -18,4 +18,59 @@ public class TodoController : ControllerBase
     {
         return TodoService.GetAll();
     }
+
+    [HttpGet("{id}")]
+    public ActionResult<Todo> Get(int id)
+    {
+        var todo = TodoService.Get(id);
+
+        if (todo == null)
+        {
+            return NotFound();
+        }
+
+        return todo;
+    }
+
+    [HttpPost]
+    public IActionResult Create(Todo todo)
+    {
+        TodoService.Add(todo);
+
+        return CreatedAtAction(nameof(Get), new { id = todo.Id }, todo);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Todo todo)
+    {
+        if (id != todo.Id)
+        {
+            return BadRequest();
+        }
+
+        var existingTodo = TodoService.Get(id);
+        if (existingTodo is null)
+        {
+            return NotFound();
+        }
+
+        TodoService.Update(todo);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var todo = TodoService.Get(id);
+
+        if (todo is null)
+        {
+            return NotFound();
+        }
+
+        TodoService.Delete(id);
+
+        return NoContent();
+    }
 }
