2f991a01dfd10b56116b1631b1c8499785ab47ca
DANSKI423319 <danielskillman357@yahoo.co.uk>
2025-05-16 15:32:52 +0100

    Add migrations to make changes to todo entities

--- Infrastructure/Migrations/20250516142108_UpdateTodoEntity.Designer.cs
+++ Infrastructure/Migrations/20250516142108_UpdateTodoEntity.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace dotnet_todos.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250516142108_UpdateTodoEntity")]
+    partial class UpdateTodoEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Migrations/20250516142108_UpdateTodoEntity.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_todos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTodoEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Todos");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Todos",
+                newName: "Task");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompletedAt",
+                table: "Todos",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Todos",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Todos",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Todos",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletedAt",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Todos");
+
+            migrationBuilder.RenameColumn(
+                name: "Task",
+                table: "Todos",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Todos",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Todos",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
