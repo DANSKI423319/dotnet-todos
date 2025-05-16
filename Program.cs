34bfb33d6616556965fec184437b09a749ea6f6c
DANSKI423319 <danielskillman357@yahoo.co.uk>
2025-05-16 15:14:22 +0100

    Update program cs to register db context

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
+using Core.Services;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register database context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("Default"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("Default"))
+    ));
+
+// Register services that user database context
+builder.Services.AddScoped<TodoService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
